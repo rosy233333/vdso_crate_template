3c03624b82445bdee29f9edc67bd5a4ad80a6b8b
rosy <lc_rosy@qq.com>
2025-08-21 11:01:29 +0800

    为适应AsyncOS上的运行而做修改：
    - 修改vvar地址的获取方式，现在不会缓存vvar地址，而是每次通过get_data_base函数获取。
        - 该修改的目的是为了统一多地址空间共享vdso的情况下，初始化函数的逻辑。现在只需要一次（vdso内的）初始化，就可在所有地址空间中使用。
    - 增加了几个示例函数，用于测试多个地址空间对共享区域的读写。

--- user_test/src/main.rs
+++ user_test/src/main.rs
@@ -8,10 +8,10 @@ fn main() {
     log::info!("Starting VDSO test...");
     let map = map_vdso().expect("Failed to map VDSO");
     init();
-    let example: ArgumentExample = api_example();
+    let example: ArgumentExample = get_example();
     assert!(
         example.i == 42,
-        "Expected api_example() to return 42, got {}",
+        "Expected get_example() to return 42, got {}",
         example.i
     );
     println!("Test passed!");
